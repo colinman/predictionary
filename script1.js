5b56c02d892da1acaf436820e303e4fc0db1c082
Brendon Go <Brendon@Clostrum.local>
2014-01-25 05:59:16 -0800

    Changed frequency to send all at the end instead

--- script1.js
+++ script1.js
@@ -9,6 +9,8 @@ var n = 3;
 var fs = require('fs'); //allows access to fs.readFile function
 var path = require('path'); //allows access to path.join function
 
+var frequency = new Array();
+
 var filePath = path.join(__dirname + '/start.html'); //this is place holder for debugging and trial, we will accept filePath from database that calls this function
 
 fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){ //opens file
@@ -28,25 +30,35 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){ //opens file
 		window.shift(); //same effect as dequeue: first element is deleted and everything shifts left
 		window.push(nextWord);
 	    }
-
+	    sendFrequency(frequency);
 	    console.log("Done");
       	}else{
 	    console.log(err);
 	}
 });
 
+/*Returns word at array[i] and increments frequency of word */
 function getWord(array, i) {
     var word = array[i];
     trackFrequency(word);
     return word;
 }
 
+/*Increments frequency of word in array */
 function trackFrequency(word) {
-    var message = "Frequency: " + word;
-    console.log(message);
+    frequency[word] = isNaN(frequency[word]) ? 1 : frequency[word] + 1;
+    var message = "Frequency of " + word + " was changed to " + frequency[word];
+    console.log(message); //there is a bug when end of line is read maybe add endline to delimiters
 }
 
+/* Debugging placeholder code. This will be implemented by database. Sends database array of n words and the word that follows*/
 function trackMapping(window, nextWord){
     var message = window + ".... maps to.... " + nextWord;
     console.log(message);
+}
+
+/* Debugging placeholder code. This will be implemented by database. Sends database array of words with their frequency  */
+function sendFrequency(frequency) {
+    console.log("Frequencies sent.");
+    console.log(frequency);
 }
