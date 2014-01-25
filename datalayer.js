d23c9aa4d6650e97c1926ffddc18da1f91269d95
Ubuntu <ubuntu@ip-172-31-36-49.us-west-2.compute.internal>
2014-01-25 20:46:14 +0000

    fixed datalayer function to return user frequency as well

--- datalayer.js
+++ datalayer.js
@@ -5,12 +5,26 @@ var fdict_topten_prefix = function(prefix, callback) {
     var collection = db.get('words');
 
     var pattern = new RegExp('^'+ prefix);
-    collection.find({word:pattern}, {sort:{rank:1, _id:0}, limit: 10}, function(e, docs) {
+    collection.find({word:pattern}, {sort:{rank:1}, limit: 10}, function(e, docs) {
+	callback(docs);
+    });
+}
+
+var ufreq_topten_prefix = function(prefix, user, callback) {
+    var mongo = require('mongodb');
+    var monk = require('monk');
+    var db = monk('localhost:27017/predictionary');
+    var collection = db.get('user_wfreq');
+
+    var pattern = new RegExp('^'+ prefix);
+    collection.find({word:pattern, username:user}, {sort:{freq:-1}, limit: 10}, function(e, docs) {
 	callback(docs);
     });
 }
 
 module.exports.fdict_topten_prefix = fdict_topten_prefix;
+module.exports.ufreq_topten_prefix = ufreq_topten_prefix;
+
 
 // collection.find({},{},function(e,docs){
 //     console.log(docs);
