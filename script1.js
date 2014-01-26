25315fbaf086781e212e3c7aed64360ce9fbfd01
Brendon Go <brendon.go@gmail.com>
2014-01-25 16:04:49 -0800

    Final Script 1

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@
  * Takes the text file then maps an array of every n words to the next word.
  */
 
-var something = function() {
+var process_file = function(filePath, username) {
 
 	var n = 3;
 
@@ -13,12 +13,13 @@ var something = function() {
 	var db = require('./datalayer.js'); //allows access to database
 
 	var frequency = new Array();
-
-	var filePath = path.join(__dirname + '/start.html'); //this is place holder for debugging and trial, we will accept filePath from database that calls this function
+	
+//	var username;
+//	var filePath = path.join(__dirname + '/start.html');
 
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){ //opens file
 		if (!err){
-			//process data, removes punctuation, saves words into array
+		    //process data, removes punctuation, saves words into array
 		    data = data.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
 		    data = data.replace(/\n/g," ");
 		    data = data.replace(/\s{2,}/g," ");
@@ -27,20 +28,19 @@ var something = function() {
 
 		    //initializes window to first n words
 		    for (var i = 0; i < n; i++) {
-				window.push(getWord(array,i));
+			window.push(getWord(array,i)); //same effect as enqueue
 		    }
 
 		    //loops over rest of array
 		    for(var i = n; i < array.length; i++) {       
-				var nextWord = getWord(array,i);
-				trackMapping(window,nextWord);
-				window.shift(); //same effect as dequeue: first element is deleted and everything shifts left
-				window.push(nextWord);
+			var nextWord = getWord(array,i);
+			db.write_word_chunk(window,nextWord,username);
+			window.shift(); //same effect as dequeue
+			window.push(nextWord);
 		    }
-
-		    db.write_word_freq(frequency);
-		    
-		    console.log('File was processed.');
+		    //sends word frequency to database
+		    db.write_word_freq(frequency, username);
+		    console.log('File Processed');
 	    } else {
 			console.log(err);
 		}
@@ -57,6 +57,6 @@ var something = function() {
 	function trackFrequency(word) {
 	    frequency[word] = isNaN(frequency[word]) ? 1 : frequency[word] + 1;
 	}
-}
+};
 
-module.exports.something = something;
+module.exports.process_file = process_file;
