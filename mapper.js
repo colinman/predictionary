4ae3f6c154053609d79c72a066f07b208b0ec2b1
unknown <kenxu95@stanford.edu>
2014-01-26 03:32:00 -0800

    Fixed

--- mapper.js
+++ mapper.js
@@ -6,18 +6,34 @@
 
 /*Gets the last three words from script3 as an array*/
 var words = new Array();
-var nextword;
+var username;
+
+var prediction=NULL;
+function callback(words){
+	var max_freq = 0;
+	if(words.length > 0){
+		for(var i = 0; i<words.length; i++){
+			var freq = get_fdict_freq(words[i]);
+			if(freq > max_freq){
+				max_freq = freq;
+				prediction = words[i];
+			}
+		}
+	}
+}
 
 for(var i = 0; i<3; i++){
 	var word1 = words[0];
 	var word2 = words[1];
 	var word3 = words[2];
-	nextword = next_chunk_word(word1, word2, word3);
-	if(nextword != NULL){
-		break;
-	}
-	words[i] = "";
+	next_chunk_word(word1, word2, word3, username, callback);
+	words[i] = NULL;
 }
 
-/*next word is valid: JSon it to the browser*/
+if(prediction != NULL){ 
+/*prediction is valid: JSon it to the browser*/
+}
+else{
+	return NULL;
+}
 
