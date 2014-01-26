9c842c52b881924dbf43f850a32b23dad6110f79
Ubuntu <ubuntu@ip-172-31-36-49.us-west-2.compute.internal>
2014-01-26 10:37:19 +0000

    asdf

---
+++ test.js
@@ -0,0 +1,24 @@
+var dl = require("./datalayer.js");
+var script1 = require("./script1.js");
+
+
+
+script1.process_file("/sampleessay.txt", "colinman@stanford.edu");
+
+//var array = {"words":2, "asdf":3};
+
+//dl.write_word_freq(array, "colinman@stanford.edu");
+
+/*
+function print(text) {
+    console.log(text);
+}
+
+var array = ["brie", "the", "argh"];
+
+//dl.write_word_chunk(array, "anotherword", "colinman@stanford.edu");
+//dl.write_word_freq(array, "colinman@stanford.edu");
+//dl.fdict_topten_prefix("bri", print);
+//dl.ufreq_topten_prefix("bler", "colinman@stanford.edu", print);
+//dl.get_fdict_freq("malina", print);
+dl.find_word_chunk("brie", "the", "argh", "colinman@stanford.edu", print);*/
